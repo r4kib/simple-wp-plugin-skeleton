80fd456f7b6743577b24e876b1cfa3790f020e7e
rakib <rakib@previewtechs.com>
2017-11-12 15:16:39 +0600

    Initial commit

---
+++ simple-wp-plugin-skeleton.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: Simple Wp Plugin Skeleton
+Plugin URI: http://example.com
+Description: This Plugin Skeleton helps quickly develop WordPress Plugin
+Version: 0.1
+Author: Rakib Hasan
+Author URI: https://rakibh.com
+License: GPL2
+*/
+
